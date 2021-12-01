0965a3eeba82d1e6c8ac81facf7b61f08415d8a0
unknown <dtm190000@utdallas.edu>
2021-11-30 19:11:46 -0600

    added main

--- main.py
+++ main.py
@@ -1,127 +1,158 @@
-import matplotlib.patches
-import yfinance as yf
-import pandas as pd
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import os
+import pandas as pd
+import sys
+
 import talib
 
-import RESETIMGDIR
-import convolve
-import maxPooling
-
-
-def saveCandlestickPattern(hist, numCandles, pattern):
-    x = range(0,len(hist))
-
-    # Creates a new matplot figure
-    fig, ax = plt.subplots(nrows=1, ncols=1)
-
-    # Directly iterates over where the candlesticks are
-    for idx_, val_ in hist.loc[idx - (numCandles - 1):idx].iterrows():
-        # Plots the low/high lines at on the x axis
-        plt.plot([x[idx_], x[idx_]], [val_['Low'], val_['High']], color='black')
-
-        # Determines whether the candle is white or black
-        if (val_["Close"] - val_["Open"] > 0):
-            curColor = "green"
-        else:
-            curColor = "red"
-
-        # Adds the candle element to the candle
-        rect = matplotlib.patches.Rectangle((x[idx_] + -.1, val_['Open']), .2, (val_['Close'] - val_['Open']),
-                                             color=curColor, zorder=10)
-        ax.add_patch(rect)
-
-    # Sees how many figures are in the pattern folder
-    num = len(os.listdir("IMGDIR/" + pattern))
-
-    # Saves the image as a new file
-    name = "IMGDIR/" + pattern + "/" + str(num) + ".jpg"
-    plt.savefig(name, bbox_inches = "tight")
-
-    # Closes the figure
-    plt.close(fig)
-
-
-# def displayPattern(temp, numCandles):
-#     x = range(0, len(hist))
-#     fig, ax = plt.subplots(nrows=1, ncols=1)
-#     ax.set_facecolor((.9, .7, .7))
-#     for idx, val in temp.iterrows():
-#         for iter in range(0, numCandles):
-#             plt.plot([x[iter], x[iter]],
-#                      [val['Low ' + str(iter)], val['High ' + str(iter)]],
-#                      color='black')
-#             if (val["Close " + str(iter)] - val["Open " + str(iter)] > 0):
-#                 curColor = "white"
-#             else:
-#                 curColor = "black"
-#             rect1 = matplotlib.patches.Rectangle((x[iter] + -.1, val['Open ' + str(iter)]), .2, (val['Close ' + str(iter)] - val['Open ' + str(iter)]),
-#                                                  color=curColor, zorder=10)
-#             ax.add_patch(rect1)
-#         # plt.show()
-#         plt.savefig("C:\\Users\\David\\PycharmProjects\\CandlestickML\\IMG3BLACKCROWS\\test.jpg")
-#
-#         fig, ax = plt.subplots(nrows=1, ncols=1)
-#         ax.set_facecolor((.9, .7, .7))
+import acquireCandlestickCharts
+import DIRHELP
+import testModel
+import trainModel
 
 if __name__ == "__main__":
-    # path = "IMGDIR/CDL3BLACKCROWS/1.jpg"
-    # filter = 0
-    # # kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-    # kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
-    # filteredImage = convolve.convolve(path, kernel)
-    # path = "IMGDIR/CDL3BLACKCROWS/1A.jpg"
-    # maxPooling.maxPooling(path, size = 2, stride = 2)
-    #
-    # path = "IMGDIR/CDL3BLACKCROWS/1B.jpg"
-    # # kernel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
-    # filteredImage = convolve.convolve(path, kernel)
-    # path = "IMGDIR/CDL3BLACKCROWS/1A.jpg"
-    # maxPooling.maxPooling(path, size = 4, stride = 4)
+    # np.set_printoptions(threshold=sys.maxsize)
 
-
+    # Whether or not you want to acquire candlestick charts
 
     # 1 if you want to clear all the files in IMGDIR and remake the IMGDIR
     if (0):
-        RESETIMGDIR.reset()
-        exit(0)
+        DIRHELP.reset()
 
-    # Sets the ticker
-    spy = yf.Ticker("SPY")
+    # AT BRT
+    if (0):
+        acquireCandlestickCharts.acquire()
 
-    # Opens the historical data as a df
-    hist = pd.read_csv("historical.csv")#pd.DataFrame(spy.history(period="max"))
+    # For testing trained models
+    if (0):
+        testModel.test()
 
-    # Drops unnecessary columns
-    # hist = hist.drop(columns=["Volume", "Dividends", "Stock Splits"])
+    # Stores all of the images in Mx28x28
+    #  M is the total number of images
+    #  28x28 is the number of pixels in each image
+    images=np.zeros((28,28))
 
-    # Changes the index from dates to numbers
-    hist.reset_index(inplace=True)
+    # Labels stores all of the labels for the images in 1xM
+    labels=np.zeros((1))
+    dir = "IMGDIR/"
 
-    # Identifies candle patterns in historical data
-    # Adds several new columns for each pattern
-    #  When a pattern is identified, it will add a -100 or 100 for downward trends or positive trends
-    #  Otherwise, the value will be 0
-    candle_names = talib.get_function_groups()['Pattern Recognition']
-    op = hist['Open']
-    hi = hist['High']
-    lo = hist['Low']
-    cl = hist['Close']
-    for candle in candle_names:
-        hist[candle] = getattr(talib, candle)(op, hi, lo, cl)
+    # These are necessary because some of the candlestick patterns
+    #  such as Evening Star is MUCH more frequent than Abandoned Baby
+    MNOIPC = 1000 # Max Number of Images Per Class
+    CNOI = 0 # Current Number of Images Per Class
+    NF = 0 # Number of folders
+    for folder in os.scandir(dir):
+        print(folder.name)
+        CNOI = 0
+        NF += 1
+        if (NF > 5):
+            break
+        for file in os.scandir(folder):
+            # If we have reached the max number of files in this folder, we will skip to the next one
+            if MNOIPC == CNOI:
+                break
 
-    # Detects when a candle pattern has been identified and saves the pattern as an image
-    for idx, val in hist.iterrows():
-        if (val['CDL3BLACKCROWS'] != 0):
-            saveCandlestickPattern(hist, 3, 'CDL3BLACKCROWS')
-        if (val['CDL3LINESTRIKE'] != 0):
-            if (val['CDL3LINESTRIKE'] == 100):
-                saveCandlestickPattern(hist, 3, 'CDL3LINESTRIKEBULL')
-            elif (val['CDL3LINESTRIKE'] == -100):
-                saveCandlestickPattern(hist, 3, 'CDL3LINESTRIKEBEAR')
-        if (val['CDLEVENINGSTAR'] != 0):
-            saveCandlestickPattern(hist, 0, 'CDLEVENINGSTAR')
-        if (val['CDLSTICKSANDWICH'] != 0):
-            saveCandlestickPattern(hist, 0, 'CDLSTICKSANDWICH')
+            # Formats the current file into something readable for cv2
+            path = "IMGDIR/" + folder.name + "/" + file.name
+
+            # Reads the current image with cv2 as grayscale
+            current = cv2.imread(path, 0)
+
+            # Resizes the image to be 28x28
+            resize = cv2.resize(current, (28, 28), interpolation=cv2.INTER_AREA)
+
+            # Appends the current image onto the Mx28x28 array
+            images = np.dstack((images, resize))
+
+            # Appends the current label onto the Mx1 array
+            labels = np.append(labels, folder.name)
+
+            CNOI+=1
+
+    # Tranposes the images from 28x28xM to Mx28x28
+    images = np.transpose(images, (2, 0, 1))
+
+    # Arrays were formatted with np.zeros, removes that single entry
+    images = np.delete(images, 0, 0)
+    labels = np.delete(labels, 0)
+
+    # Turns the classes into unique numbers
+    labels = pd.factorize(labels)[0]
+
+    # Turns images into floats
+    images = images/255 - .5
+
+    # Train Test Split
+    trainPercent = .8 # 80/20 split
+    trainImages, trainLabels, testImages, testLabels = trainModel.trainTestSplit(images, labels, trainPercent)
+
+    # Shows the dimensions of the training and testing arrays
+    print(trainImages.shape)
+    print(trainLabels.shape)
+    print(testImages.shape)
+    print(testLabels.shape)
+
+    # Kernels for convolutions
+    kernel1 = np.random.uniform(low=-.1, high=.1, size=(4, 3, 3))
+    kernel2 = np.random.uniform(low=-.1, high=.1, size=(6, 4, 3, 3))
+
+    # For this calculation, see the bottom
+    weightDim = (int((int((int(((28 - kernel1.shape[1] + 1) / 2)) - kernel2.shape[2] + 1) / 2)) ** 2) * kernel2.shape[0])
+
+
+    # Weights for the fully connected layer
+    # Weights are MxN where:
+    #  M is the number of classes
+    #  N is the number of pixels in the final, flattened convolved/pooled image
+    weights = np.random.uniform(low=-.1, high=.1, size=(NF, weightDim))
+
+    numEpochs = 10
+    learningRate = .005
+
+    # Trains the model!
+    epochs, avgLosses, accuracy, kernel1, kernel2, weights = trainModel.train(trainImages, trainLabels, testImages,
+                                                                              testLabels, numEpochs, learningRate,
+                                                                              kernel1, kernel2, weights)
+    print(repr(kernel1))
+    print("kernel1")
+    print("-----------------")
+    print(repr(kernel2))
+    print("kernel2")
+    print("-----------------")
+    print(repr(weights))
+    print("Weights")
+    print("-----------------")
+    print(avgLosses)
+    print("avgLosses")
+    print("-----------------")
+    print(accuracy)
+    print("accuracy")
+    print("-----------------")
+    exit(0)
+
+    # Example of calculating the weight dimensions
+    #
+    # 28 - 5 height + 1 / 2
+    # For kernel1 = 1, 5, 5
+    #     kernel2 = 1, 1, 5, 5
+    # N is calculated as follows:
+    #  C1
+    #  (original pixel height) - (kernel1 height) + 1
+    #  28 - 5 + 1 = 24
+    #
+    #  P1
+    #  C1 height / 2
+    #  24 / 2 = 12
+    #
+    #  C2
+    #  (P1 pixel height) - (kernel2 height) + 1
+    #  12 - 5 + 1 = 8
+    #
+    #  P2
+    #  (C2 height / 2)
+    #  8 / 2 = 4
+    #
+    #  Thus, image size is 4x4
+    #  Final height * Final width * Number of k2 kernels
+    #  4 * 4 * 1 = 16
